c7256d04ca9af654a0979127514df960f94e2fca
Gijsvanderveen <152379@csgaugustinus.nl>
2020-11-12 12:00:16 +0000

    dr

--- JS/H1/H1O01.js
+++ JS/H1/H1O01.js
@@ -1,6 +1,6 @@
 function setup() {
-  canvas = createCanvas(1000,500);
-  background('orange');
+  canvas = createCanvas(1000,450);
+  background('grey');
   canvas.parent('processing');
   noLoop();
 }
@@ -10,12 +10,12 @@ function draw() {
   
   noStroke();
   fill('green');
-  ellipse(300,200,400);
+  ellipse(500,200,350);
   
   // witte rechthoek met rode rand
   
-  stroke('red');
+  stroke('green');
   fill('white');
   strokeWeight(10);
-  rect(650,100,250,300);  
+  rect(380,200,250,300);  
 }
