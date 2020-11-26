0a3350cd8bfad9b362eddd979ba05d63d8ad2c2f
Gijsvanderveen <152379@csgaugustinus.nl>
2020-11-26 09:09:18 +0000

    ihiaHOI

--- JS/H1/H1O03.js
+++ JS/H1/H1O03.js
@@ -3,16 +3,16 @@ function setup() {
   background('white');
   canvas.parent('processing');
   noLoop();
-  // colorMode(RGB,255,255,255,1);  
+  colorMode(RGB,255,255,255,1);  
 }
 
 function draw() {
   noStroke();
-  // fill(0,0,255,0.3);
+  fill(0,0,255,0.3);
   fill('red');
   rect(0,150,450,150);
-  fill('green');
-  rect(0,0,150,450);
-  fill('blue');  
+  fill('#008000');
+  rect(0,255,0);
+  fill('#0000FF');  
   rect(75,0,375,450);
 }
